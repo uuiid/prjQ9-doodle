cd391a5b262ff0e18dcc2d7bfcd36a7884107321
texiao_zhang <texiao_zhang>
2020-01-19 20:49:55 +0800

    提取目录

--- doodleBrowser.py
+++ doodleBrowser.py
@@ -1,8 +1,11 @@
 from PyQt5 import QtGui,QtCore,QtWidgets
 import os
 import sys
+import pathlib
+import doodleCore
 from mainUI.mainWindow import Ui_MainWindow
 
+test = pathlib.Path(r"W:\03_Workflow\Shots")
 
 class MainDoodle(Ui_MainWindow,QtWidgets.QMainWindow):
     def __init__(self):
@@ -11,13 +14,15 @@ class MainDoodle(Ui_MainWindow,QtWidgets.QMainWindow):
         self.pushButton.clicked.connect(self.browseFolder)
 
 
-    def browseFolder(self):
-        self.treeWidget.clear()
-        dirextory = QtWidgets.QFileDialog.getExistingDirectory(self,"pushButton")
-
-        if dirextory:
+    def browseFolder(self,shots:str):
+        #self.treeWidget.clear()
+        #dirextory = QtWidgets.QFileDialog.getExistingDirectory(self,"pushButton")
+        shots = doodleCore.DoodleCore.getShots(test)
+        if shots:
             root = QtWidgets.QTreeWidgetItem(self.treeWidget)
-            for fileName in os.listdir(dirextory):
+            for fileName in shots:
+                if fileName.split("-")[-1]:
+
                 item = QtWidgets.QTreeWidgetItem(root)
                 item.setText(0,fileName)
                 root.addChild(item)
--- doodleCore.py
+++ doodleCore.py
@@ -1,21 +1,48 @@
 import pathlib
-import os
+import os,sys
 import platform
+import doodleBrowser
+from PyQt5 import QtGui,QtCore,QtWidgets
 
-class doodleCore:
+test = pathlib.Path(r"W:\03_Workflow\Shots")
+
+DOODLEROOT = pathlib.Path(__file__).parent.expanduser()
+
+
+class DoodleCore:
     def __init__(self):
         self.doodleIni = ""
 
         try:
             self.version = "v0.1"
-            self.doodleRoot = pathlib.Path(__file__)
+            self.doodleRoot = DOODLEROOT
 
             if platform.system() == "Windows":
-                self.userini = ppathlib.Path.home().joinpath('Documents','doodle','doodle.json')
+                self.userini = pathlib.Path.home().joinpath('Documents','doodle','doodle.json')
         except:
             print("no")
 
+    @classmethod
+    def getShots(self,shotsDir:pathlib.Path):
+        tmpShots = shotsDir.iterdir()
+        shots =[]
+        for shot in tmpShots:
+            if shot.is_dir():
+                shots.append(shot.stem)
+        return shots
+
+    # @classmethod
+    # def test(cls):
+    #     print("ok test")
 
 
 if __name__ == "__main__":
-    print(doodleCore.doodleRoot)
+    #DoodleCore.test()
+    testdood = DoodleCore()
+    print(testdood.doodleRoot)
+    print(test)
+    print(DoodleCore.getShots(test))
+    # app = QtWidgets.QApplication(sys.argv)
+    # MainWidow = doodleBrowser.MainDoodle()
+    # MainWidow.show()
+    # app.exec_()
